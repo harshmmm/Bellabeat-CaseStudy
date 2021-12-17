1fd390724925eb3ffb52d39d51c46621e3432250
harshmmm <67603170+harshmmm@users.noreply.github.com>
2021-12-17 16:44:16 +0530

    Update bellabeat_analysis.R

--- bellabeat_analysis.R
+++ bellabeat_analysis.R
@@ -1,7 +1,7 @@
-#load libraries and dependancies
+#load libraries and dependencies
 
 library(tidyverse) #helps wrangle data
 library(ggplot2) #for visualuization plots
-library(dplyr)
-library(tidyr)
+library(dplyr) #for dataframe manipulation
+library(tidyr) #to create tidy data
 library(lubridate) #helps work on date-time data
